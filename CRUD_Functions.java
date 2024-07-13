e9dbfd164a9c60c6e660fab7b4de9fef68bf0080
Nalankumar <Nalankumar@users.noreply.github.com>
2024-07-13 07:59:13 +0530

    Source files uploaded

---
+++ CRUD_Functions.java
@@ -0,0 +1,189 @@
+package main;
+
+import java.sql.*;
+public class CRUD_Functions {
+	private static Connection makeConnection() throws Exception{
+		String url = "jdbc:mysql://localhost:3306/lbms";
+		String uname = "root";
+		String password = "TNK_sql_007";
+		Connection con = DriverManager.getConnection(url, uname, password);
+		return con;
+	}
+
+	public void addBook(String name, String author) throws Exception{
+		Connection con = makeConnection();
+		String query = "insert into books(bname, author) values(?,?)";
+		PreparedStatement st = con.prepareStatement(query);
+		st.setString(1, name);
+		st.setString(2, author);
+		int rs = st.executeUpdate();
+		if(rs == 1) {
+			System.out.println("Book successfully inserted");
+		}	
+	}
+	
+	public void removeBook(int id) throws Exception{
+		Connection con = makeConnection();
+		String query = "delete from books where bid = ?";
+		PreparedStatement st = con.prepareStatement(query);
+		st.setInt(1, id);
+		int rs = st.executeUpdate();
+		if(rs == 1) {
+			System.out.println("Book was removed successfully!");
+		}
+	}
+	
+	public void readBooks() throws Exception{
+		Connection con = makeConnection();
+		String query = "select * from books";
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(query);
+		while(rs.next()) {
+			System.out.println("\nBook ID: " + rs.getInt(1) + "\nBook name:" + rs.getString(2) + "\nAuthor name: " + rs.getString(3) + "\nBorrowed by: "+rs.getInt(4));
+		}	
+	}
+	
+	public void updateName(int bid ,String name) throws Exception{
+		Connection con = makeConnection();
+		String query = "update books set bname= ? where bid = ?";
+		PreparedStatement st = con.prepareStatement(query);
+		st.setString(1, name);
+		st.setInt(2, bid);
+		int rs = st.executeUpdate();
+		if(rs == 0) {
+			System.out.println("Book name updated!");
+		}
+		else
+		{
+			System.out.println("Book name does not updated!");
+		}
+		
+	}
+	public void updateAuthor(int bid ,String aname) throws Exception{
+		Connection con = makeConnection();
+		String query = "update books set author = ? where bid = ?";
+		PreparedStatement st = con.prepareStatement(query);
+		st.setString(1, aname);
+		st.setInt(2, bid);
+		int rs = st.executeUpdate();
+		if(rs == 0) {
+			System.out.println("Book name updated!");
+		}
+		else
+		{
+			System.out.println("Book name does not updated!");
+		}
+		
+	}
+	
+	public boolean checkAvailability(int id) throws Exception{
+		Connection con = makeConnection();
+		String query = "select borrower_id from books where bid = ?";
+		PreparedStatement st = con.prepareStatement(query);
+		st.setInt(1, id);
+		ResultSet rs = st.executeQuery();
+		while(!rs.next()) {
+			return false;
+		}
+		return true;
+	}
+	
+	public void borrowBook(int bid, int sid) throws Exception{
+		if(checkAvailability(bid)) {
+		Connection con = makeConnection();
+		String query = "update books set borrower_id = ? where bid = ?";
+		PreparedStatement st = con.prepareStatement(query);
+		st.setInt(1, sid);
+		st.setInt(2, bid);
+		st.executeUpdate();
+		System.out.println("Book borrowed successfrully");
+		}
+		else {
+			System.out.println("Book is not available now!");
+		}
+	}
+
+	public void returnBook(int bid, int sid) throws Exception{
+		
+		Connection con = makeConnection();
+		String query = "update books set borrower_id = ? where bid = ?";
+		PreparedStatement st = con.prepareStatement(query);
+		st.setNull(1, Types.INTEGER);
+		st.setInt(2, bid);
+		st.executeUpdate();
+		System.out.print("Book returned successfully");
+		
+		
+	}
+
+	public void searchByName(String bname) {
+	    try {
+	        Connection con = makeConnection();
+	        String query = "SELECT * FROM books WHERE bname = ?";
+	        PreparedStatement st = con.prepareStatement(query);
+	        st.setString(1, bname);
+	        ResultSet rs = st.executeQuery();
+	        if (!rs.isBeforeFirst()) {
+	            System.out.println("No books found with the name: " + bname);
+	        } else {
+	            while (rs.next()) {
+	                System.out.println("\nBook ID: " + rs.getInt(1) + "\nBook name: " + rs.getString(2) + "\nAuthor name: " + rs.getString(3) + "\nBorrowed by: " + rs.getInt(4));
+	            }
+	        }
+	     
+	        rs.close();
+	        st.close();
+	        con.close();
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	}
+	
+	public void searchById(int id) {
+	    try {
+	        Connection con = makeConnection();
+	        String query = "SELECT * FROM books WHERE bid = ?";
+	        PreparedStatement st = con.prepareStatement(query);
+	        st.setInt(1, id);
+	        ResultSet rs = st.executeQuery();
+	        if (!rs.isBeforeFirst()) {
+	            System.out.println("No books found with the name: " + id);
+	        } else {
+	            while (rs.next()) {
+	                System.out.println("\nBook ID: " + rs.getInt(1) + "\nBook name: " + rs.getString(2) + "\nAuthor name: " + rs.getString(3) + "\nBorrowed by: " + rs.getInt(4));
+	            }
+	        }
+	       
+	        rs.close();
+	        st.close();
+	        con.close();
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	}
+		
+	    public void searchByAuthor(String author) {
+		    try {
+		        Connection con = makeConnection();
+		        String query = "SELECT * FROM books WHERE author = ?";
+		        PreparedStatement st = con.prepareStatement(query);
+		        st.setString(1, author);
+		        ResultSet rs = st.executeQuery();
+		        if (!rs.isBeforeFirst()) {
+		            System.out.println("No books found with the author: " + author);
+		        } else {
+		            while (rs.next()) {
+		                System.out.println("\nBook ID: " + rs.getInt(1) + "\nBook name: " + rs.getString(2) + "\nAuthor name: " + rs.getString(3) + "\nBorrowed by: " + rs.getInt(4));
+		            }
+		        }
+		       
+		        rs.close();
+		        st.close();
+		        con.close();
+		    } catch (Exception e) {
+		        e.printStackTrace();
+		    }
+		
+	    }
+
+}
---
+++ Main.java
@@ -0,0 +1,169 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		Scanner sc = new Scanner(System.in);
+		boolean flag = true;
+		int choice, bid;
+		String aname, bname;
+		CRUD_Functions cf = new CRUD_Functions();
+		while(flag) {
+			System.out.print("1. Librarian\n2. Student\n0. Exit\nEnter your choice: ");
+			choice = sc.nextInt();
+			switch(choice) {
+				case 0:
+					flag = false;
+					System.out.println("Thank you...");
+					break;
+				case 1:
+					System.out.print("1. Add book\n2. Remove book\n3. View books\n4. Update book\n5. Search books\n0. Go back\nEnter your choice: ");
+					choice = sc.nextInt();
+					sc.nextLine();
+					switch(choice){
+						case 1:
+							System.out.print("Enter book name: ");
+							bname = sc.nextLine();
+							System.out.print("Enter author name: ");
+							aname = sc.nextLine();
+							cf.addBook(bname, aname);
+							break;
+						case 2:
+							System.out.print("Enter book id to remove: ");
+							bid = sc.nextInt();
+							cf.removeBook(bid);
+							break;
+						case 3:
+							System.out.println("All available books in library: ");
+							cf.readBooks();
+							break;
+						case 4:
+							System.out.println("1. Update name\n2. Update author\n0. Go back\nEnter your choice: ");
+							choice = sc.nextInt();
+							switch(choice) {
+								case 1:
+									System.out.println("Enter id to update name: ");
+									bid = sc.nextInt();
+									sc.nextLine();
+									System.out.println("Enter name to update: ");
+									String name = sc.nextLine();
+									cf.updateName(bid, name);
+									break;
+								case 2:
+									System.out.println("Enter id to update author: ");
+									bid = sc.nextInt();
+									sc.nextLine();
+									System.out.println("Enter author to update: ");
+									aname = sc.nextLine();
+									cf.updateAuthor(bid, aname);							
+									break;
+							}
+						break;
+						case 5:
+							System.out.print("1. Search by id\n2. Search by name\n3. Search by author\n4. Go back\nEnter your choice: ");
+							choice = sc.nextInt();
+							sc.nextLine();
+							switch(choice) {
+								case 1:
+									System.out.print("Enter id to search book: ");
+									bid = sc.nextInt();
+									cf.searchById(bid);
+									break;
+								case 2:
+									System.out.println("Enter name to search book:");
+									bname = sc.nextLine();
+									cf.searchByName(bname);
+									break;
+								
+								case 3:
+									System.out.println("Enter author name to search books:");
+									aname = sc.nextLine();
+									cf.searchByAuthor(aname);
+									break;
+								case 4:
+									break;
+								default:
+									System.out.println("Enter valid choice!");
+							}
+						
+						break;
+					default:
+						System.out.println("Enter valid choice!");
+					}
+					break;	
+				case 2:
+					System.out.print("1. View books\n2. Borrow book\n3. Return book\n4. Check Availability of book\n5. Search\n0. Go back\nEnter your choice: ");
+					choice = sc.nextInt();
+					switch(choice) {
+						case 0:
+							break;
+						case 1:
+							System.out.println("Available books in library: ");
+							cf.readBooks();
+							break;
+						case 2:
+							System.out.println("Enter your id: ");
+							int sid = sc.nextInt();
+							System.out.print("Enter book id to borrow book: ");
+							bid = sc.nextInt();
+							cf.borrowBook(bid, sid);
+							break;
+						case 3:
+							System.out.println("Enter your id: ");
+							sid = sc.nextInt();
+							System.out.print("Enter book id to return book: ");
+							bid = sc.nextInt();
+							cf.returnBook(bid, sid);
+							break;
+						case 4:
+							System.out.print("Enter book id to check availabity: ");
+							bid = sc.nextInt();
+							if(cf.checkAvailability(bid)) {
+								System.out.println("The book is unavailable");
+
+							}
+							else {
+								System.out.println("The book is available!");
+							}
+							break;
+						case 5:
+							System.out.print("1. Search by id\n2. Search by name\n3. Search by author\n4. Go back\nEnter your choice: ");
+							choice = sc.nextInt();
+							sc.nextLine();
+							switch(choice) {
+								case 1:
+									System.out.print("Enter id to search book: ");
+									bid = sc.nextInt();
+									cf.searchById(bid);
+									break;
+								case 2:
+									System.out.println("Enter name to search book:");
+									bname = sc.nextLine();
+									cf.searchByName(bname);
+									break;
+									
+								case 3:
+									System.out.println("Enter author name to search books:");
+									aname = sc.nextLine();
+									cf.searchByAuthor(aname);
+									break;
+								case 4:
+									break;
+								default:
+									System.out.println("Enter valid choice!");
+							}
+							break;
+						default:
+							System.out.println("Enter valid choice!");
+					}
+					
+					break;
+					default:
+						System.out.println("Enter valid choice!");
+			}
+			
+		}
+		sc.close();
+	}
+}
